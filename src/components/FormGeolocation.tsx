d8216728b7d7681289d398acfaeddd03cce28b9d
Hanka8 <hanka.maruskevicova@gmail.com>
2024-11-04 18:58:29 +0100

    Add bird icon to map

--- src/components/FormGeolocation.tsx
+++ src/components/FormGeolocation.tsx
@@ -96,7 +96,7 @@ const LocationForm: React.FC<LocationFormProps> = ({
           />
           <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600 flex-shrink-0"></div>
           <span className="ms-3 text-sm font-medium text-gray-700">
-            Insert manually
+            {geolocationManually ? "Get your location" : "Set adress manually" }
           </span>
         </label>
         <div className="m-2 flex flex-col relative">
